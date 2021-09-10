82566b35cf30f3d2177925e30b69e7e75b59f807
Stanley <swiftwind0405@gmail.com>
2021-09-10 14:26:52 +0800

    init

---
+++ apps/landing/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Thing} from '@shared/components';
+
+function App() {
+  return (
+    <div className="App">
+      <Thing/>
+
+      11
+    </div>
+  );
+}
+
+export default App;
